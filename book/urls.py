d6ac866f0958d19e27c430689099889d9fc6310c
ryotaohashi-codor <ohashi@codor.co.jp>
2020-10-27 08:15:21 +0900

    download

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import downloadview
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('download/<int:pk>', downloadview),
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import FileUpload
+from django.http import HttpResponse
+import mimetypes
+import shutil
 
 # Create your views here.
+
+def downloadview(request, pk):
+    object = get_object_or_404(FileUpload, pk=pk)
+    file = object.files
+    name = file.name
+    response = HttpResponse(content_type=mimetypes.guess_type(name)[0] or 'application/octet-stream')
+    response['Content-Disposition'] = f'attachement; filename={name}'
+    shutil.copyfileobj(file, response)
+    return response
