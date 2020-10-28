c7d6d6450679605d27b1c39f375aefed01a55fb7
ryotaohashi-codor <ohashi@codor.co.jp>
2020-10-29 06:09:14 +0900

    mail

--- book/views.py
+++ book/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 # Create your views here.
+
+def sendmail(request):
+    message = Mail(
+        from_email='support@django-academy.net',
+        to_emails='ryotaohashi.codor@gmail.com',
+        subject = 'title',
+        html_content= 'contentcontent'
+    )
+    sg = SendGridAPIClient('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')
+    response = sg.send(message)
+    return HttpResponse('email')
