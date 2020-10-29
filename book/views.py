d5587f1abef4752e9c779aa3c3b6cd8531ed751d
ryotaohashi-codor <ohashi@codor.co.jp>
2020-10-29 16:36:53 +0900

    pagination

--- book/views.py
+++ book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Book
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+def indexview(request):
+    object_list = Book.objects.all()
+    paginator = Paginator(object_list, 2)
+    page = request.GET.get('page', 1)
+    paginated_list = paginator.page(page)
+    return render(request, 'index.html', {'paginated_list':paginated_list})
