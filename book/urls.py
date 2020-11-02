d4de030d6f3a999e0a63e707bb10b035fcdc8a5c
ryotaohashi-codor <ohashi@codor.co.jp>
2020-11-02 15:48:30 +0900

    a

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import requestfunction
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('request/', requestfunction)
 ]
