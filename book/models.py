76a29f56b109db43effcff61f860bc9685192128
ryotaohashi-codor <ohashi@codor.co.jp>
2020-11-12 15:25:09 +0900

    a

--- book/models.py
+++ book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+CHOICES = ((1,'Morning'), (2,'Afternoon'))
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(blank=True, null=True)
+    content = models.TextField(default='some content')
+    author = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.title = 'some title'
+        super(Book, self).save(*args, **kwargs)
+    
+    def age(self):
+        return 'a'
+
